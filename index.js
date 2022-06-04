901ac469f02d7ad66fad25356793f0901c5c895b
mamayie <mappiah319@gmail.com>
2022-06-04 16:46:44 +0000

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code in this file!
+const currentUser = 'Mohammed Akwasi Appiah';
+const shortGreeting = `welcome, ${currentUserlearn}!`;
+//const excitedWelcomeMessage = welcomeMessage.toUpperCase();
